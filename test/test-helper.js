9ddb64c7baae50f6ee99375e5f4828b1139c1a4f
mazhekin <mazhekin@gmail.com>
2016-07-15 09:24:27 +0200

    small fix

--- test/test-helper.js
+++ test/test-helper.js
@@ -5,6 +5,7 @@ var TestHelper = function () {
     var connectDb = function(database, success) {
         if (mongoose.connection.readyState == 1) {
             success();
+            return;
         }
         mongoose.connect(database, function (err, res) {
             if (err) {
